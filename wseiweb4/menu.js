57389aba706f1fcbad10401b8e762b97a4322c09
dervitpl <kacpermucha420@gmail.com>
2021-06-16 01:39:34 +0200

    tylko lewy pasek

---
+++ wseiweb4/menu.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("lefttabid").style.width = "150px";
+    document.getElementById("zawartosc").style.marginLeft = "150px";
+  }
+  function closeNav() {
+    document.getElementById("lefttabid").style.width = "0";
+    document.getElementById("zawartosc").style.marginLeft= "0";
+  }
